14dffe3399bc8c82e25ccfcb737a8fb2cbf82263
Tarundeep Sharma <sharma.tarundeep@gmail.com>
2024-01-30 12:18:57 -0500

    added more class and object functionality

--- Python_Network_Programming/Python_Fundamentals/Classes_Objects/Classes_objects.py
+++ Python_Network_Programming/Python_Fundamentals/Classes_Objects/Classes_objects.py
@@ -11,3 +11,39 @@ class MyRouter(object):
         print("Router sno is : ", self.sno)
         print("Router ios is : ", self.ios)
         print("Router model & manufacturing date is : ", self.model + manu_date)
+
+#creating objects from a class
+router1 = MyRouter("R1", "2600", "123456","12.4")
+print(router1)
+router2 = MyRouter("R2", "7500", "00000","10.1")
+print(router2)
+
+#accessing the attributes of an object
+print(router1.routername)
+print(router1.model)
+print(router1.sno)
+print(router1.ios)
+
+#call a method which is inside a class defination
+router1.print_router("Jan01,2024")
+router2.print_router("May01,2024")
+
+#change values of an object attribute
+router1.ios = "0.0"
+print (router1.ios)
+router1.print_router("Jan01,2024")
+
+# getattr
+print (getattr(router1,"ios"))
+
+#setattr
+setattr(router1, "ios", "10.10")
+print (getattr(router1,"ios"))
+
+#hasattr
+print(hasattr(router1, "ios")) # True
+print(hasattr(router1, "IOS")) # False
+
+#isinstance - To check an object is an instance of a particular class
+print(isinstance(router2, MyRouter))
+print(isinstance(router1, MyRouter))
