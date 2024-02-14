5a5d3a3827b462890c759b738d7e11e298337958
Tarundeep Sharma <sharma.tarundeep@gmail.com>
2024-02-14 10:59:58 -0500

    started working on project 5

--- Python_Projects/Network_Application_5_Config_Mgmt_Email_Notification_For_Config_Changes/ConfigMgmt.py
+++ Python_Projects/Network_Application_5_Config_Mgmt_Email_Notification_For_Config_Changes/ConfigMgmt.py
@@ -11,14 +11,23 @@ from email.mime.text import MIMEText
 from netmiko import ConnectHandler
  
 #Defining the device to monitor
-ip = '10.10.10.2'
+ip_smm353 = '172.30.190.31'
  
-#Defining the device type for running netmiko
+#Defining the device type for running netmiko. Netmiko has multi vendor support, so telling which OS we are using will enhance its capabilities
 device_type = 'arista_eos'
  
 #Defining the username and password for running netmiko
 username = 'admin'
-password = 'python'
+password = ''
  
 #Defining the command to send to each device
 command = 'show running'
+
+#Connecting to the device via SSH, global_delay_factor is used to avoid timeout error in case of slow connections
+session = ConnectHandler(device_type = device_type, ip = ip, username = username, password = password, global_delay_factor = 3)
+ 
+#Entering enable mode
+enable = session.enable()
+ 
+#Sending the command and storing the output (running configuration)
+output = session.send_command(command)
