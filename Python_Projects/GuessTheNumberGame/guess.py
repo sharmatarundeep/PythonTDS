a366dbd9aab84df77c1d360bd1619feef8a6fd6b
Tarundeep Sharma <sharma.tarundeep@gmail.com>
2024-02-24 06:53:17 -0500

    created a fun game

---
+++ Python_Projects/GuessTheNumberGame/guess.py
@@ -0,0 +1,24 @@
+# This is a fun guess the number game
+import random # as we need to come up with a random number, we would need random module
+
+print("Hi, welcome to the Guess the number game...\nWhat is your name?")
+name = input() # Take user input
+
+print("Well " + name + ", I am thinking of a number between 1 to 20, can you guess it?")
+secretnumber = random.randint(1,20) # Generate a random number and store it to compare later
+
+#Ask player to guess 5 times. 5 is the max chances player have to guess it right
+for i in range (5):
+    print("Take a guess")
+    guess = int(input())
+    if guess > secretnumber:
+        print("Your guess is too high")
+    elif guess < secretnumber:
+        print("Your guess is too low")
+    else:
+        break # this condition is for correct guess
+
+if guess == secretnumber:
+    print("Good job, " + name + ". You guess it right in " + str(i+1)+ " times")
+else:
+    print("Sorry, you lost all your " + str(i+1) + " chances. I was thinking about " + str (secretnumber))
---
+++ Python_Projects/GuessTheNumberGame/readme.md
@@ -0,0 +1,8 @@
+# Just a fun game
+* Ask someone to guess a number within a range. 
+* Once the person starts guessing give some hints like no your guess is too high or low to help person guess the right number. 
+* But make sure you don't give infinite chances to guess example a person will get just 5 chances Max. 
+* Once the person guess the number right congratulate and tell how many tried he/she took to guess it
+
+<br>
+Author - Tarundeep Sharma
